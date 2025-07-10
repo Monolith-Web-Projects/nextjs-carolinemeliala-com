1c67418f50e93865b3a42326790fea29c603292d
Rocky Daniel <48907996+Rockydaniel@users.noreply.github.com>
2025-07-10 21:55:37 +0700

    - adding selective urlConfig for dev & prod

--- src/lib/urlConfig.ts
+++ src/lib/urlConfig.ts
@@ -1,3 +1,6 @@
 export const urlConfig = {
-    apiBaseUrl: "http://localhost:8000/api",
+  apiBaseUrl:
+    process.env.NODE_ENV === 'production'
+      ? process.env.NEXT_PUBLIC_API_URL || 'http://django:8000/api' // Docker internal host
+      : process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api',
 };
